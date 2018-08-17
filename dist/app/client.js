8cc10493534d73addaaab297f5c39b93b168c7c8
Kyle Davidson <kyle.davidson@digital.landregistry.gov.uk>
2018-08-17 16:00:22 +0100

    Get endpoint

--- dist/app/client.js
+++ dist/app/client.js
@@ -70,6 +70,11 @@ class StreetManagerApiClient {
             return this.httpHandler(() => this.axios.post(`/works/${referenceNumber}/reinstatements`, reinstatementCreateRequest));
         });
     }
+    getReinstatement(referenceNumber, reinstatementId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return this.httpHandler(() => this.axios.get(`/works/${referenceNumber}/reinstatements/${reinstatementId}`));
+        });
+    }
     httpHandler(request) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
