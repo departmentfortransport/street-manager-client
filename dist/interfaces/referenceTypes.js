e288e776ffef382718e28783741c2ff8d04f7cb7
Peadar Kelly <peadark@Systemss-MacBook-Pro-2.local>
2018-09-13 13:54:06 +0100

    Updated PermitStatus and AssessmentStatus to align with new values

--- dist/interfaces/referenceTypes.js
+++ dist/interfaces/referenceTypes.js
@@ -52,12 +52,20 @@ var WorkCategory;
 var PermitStatus;
 (function (PermitStatus) {
     PermitStatus["submitted"] = "submitted";
-    PermitStatus["assessed"] = "assessed";
-    PermitStatus["work_in_progress"] = "work_in_progress";
+    PermitStatus["granted_proposed"] = "granted_proposed";
+    PermitStatus["granted_with_changes"] = "granted_with_changes";
+    PermitStatus["refused"] = "refused";
+    PermitStatus["granted_in_progress"] = "granted_in_progress";
+    PermitStatus["closed"] = "closed";
+    PermitStatus["cancelled"] = "cancelled";
+    PermitStatus["revoked_proposed"] = "revoked_proposed";
+    PermitStatus["revoked_in_progress"] = "revoked_in_progress";
+    PermitStatus["deemed_proposed"] = "deemed_proposed";
+    PermitStatus["deemed_in_progress"] = "deemed_in_progress";
 })(PermitStatus = exports.PermitStatus || (exports.PermitStatus = {}));
 var AssessmentStatus;
 (function (AssessmentStatus) {
-    AssessmentStatus["granted"] = "granted";
+    AssessmentStatus["granted_proposed"] = "granted_proposed";
     AssessmentStatus["granted_with_changes"] = "granted_with_changes";
     AssessmentStatus["refused"] = "refused";
 })(AssessmentStatus = exports.AssessmentStatus || (exports.AssessmentStatus = {}));
--- src/interfaces/referenceTypes.ts
+++ src/interfaces/referenceTypes.ts
@@ -49,12 +49,20 @@ export enum WorkCategory {
 
 export enum PermitStatus {
   submitted = 'submitted',
-  assessed = 'assessed',
-  work_in_progress = 'work_in_progress'
+  granted_proposed = 'granted_proposed',
+  granted_with_changes = 'granted_with_changes',
+  refused = 'refused',
+  granted_in_progress = 'granted_in_progress',
+  closed = 'closed',
+  cancelled = 'cancelled',
+  revoked_proposed = 'revoked_proposed',
+  revoked_in_progress = 'revoked_in_progress',
+  deemed_proposed = 'deemed_proposed',
+  deemed_in_progress = 'deemed_in_progress'
 }
 
 export enum AssessmentStatus {
-  granted = 'granted',
+  granted_proposed = 'granted_proposed',
   granted_with_changes = 'granted_with_changes',
   refused = 'refused'
 }
