8e38c3f2679b0ccabf09f83074129162864defb9
Catherine Paul <catherinep@systemss-MacBook-Pro.local>
2018-09-20 16:11:00 +0100

    Extended permit request and response to include permitConditions if they exist

--- src/interfaces/referenceTypes.ts
+++ src/interfaces/referenceTypes.ts
@@ -98,3 +98,22 @@ export enum DefectFixedOnsite {
   yes_by_onsite_members = 'yes_by_onsite_members',
   no = 'no'
 }
+
+export enum PermitConditionType {
+  NCT01a = 'NCT01a',
+  NCT02a = 'NCT02a',
+  NCT02b = 'NCT02b',
+  NCT04a = 'NCT04a',
+  NCT05a = 'NCT05a',
+  NCT06a = 'NCT06a',
+  NCT07a = 'NCT07a',
+  NCT08a = 'NCT08a',
+  NCT08b = 'NCT08b',
+  NCT09a = 'NCT09a',
+  NCT09b = 'NCT09b',
+  NCT09c = 'NCT09c',
+  NCT10a = 'NCT10a',
+  NCT11b = 'NCT11b',
+  NCT12a = 'NCT12a',
+  NCT13 = 'NCT13'
+}
