00ff5d567bb104c552811df5ad617dbc1f0b393a
annemca <a.mcauley@kainos.com>
2018-12-17 10:41:30 +0000

    remove offence code from fpn response and make promoter and highway authority organisations optional

--- src/interfaces/fpnResponse.ts
+++ src/interfaces/fpnResponse.ts
@@ -1,16 +1,14 @@
 import { FPNSummaryResponse } from './fpnSummaryResponse'
-import { OffenceCode } from './referenceTypes'
+import { FileResponse } from './fileResponse'
 
 export interface FPNResponse extends FPNSummaryResponse {
-  offence_code: OffenceCode,
   location: string,
   offence_date: Date,
   offence_details: string,
   authorised_officer: string,
   officer_contact_details: string,
-  fpn_evidence: boolean,
-  file_ids?: number[],
   work_reference_number: string,
-  promoter_organisation: string,
-  highway_authority: string
+  promoter_organisation?: string,
+  highway_authority?: string,
+  files?: FileResponse[]
 }
