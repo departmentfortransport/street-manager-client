89737fdc03b58d583c4ff8c68f27810453e43138
Catherine Paul <c.paul@kainos.com>
2018-12-18 16:12:01 +0000

    SM-617-1491 - Update WorkResponse to include an activePermit instead of maintaining permit details at root level

--- dist/interfaces/workResponse.d.ts
+++ dist/interfaces/workResponse.d.ts
@@ -1,22 +1,12 @@
 import { InspectionResponse } from './inspectionResponse';
 import { SiteSummaryResponse } from './siteSummaryResponse';
-import { PermitStatus } from './referenceTypes';
 import { PermitSummaryResponse } from './permitSummaryResponse';
 import { FPNSummaryResponse } from './fpnSummaryResponse';
+import { PermitResponse } from './permitResponse';
 export interface WorkResponse {
     work_id: number;
     work_reference_number: string;
-    permit_reference_number: string;
-    actual_start_date?: Date;
-    actual_end_date?: Date;
-    excavation_carried_out: boolean;
-    inspection_units?: number;
-    permit_status: PermitStatus;
-    promoter_swa_code: string;
-    highway_authority_swa_code: string;
-    works_location_description: string;
-    street_name: string;
-    area_name: string;
+    active_permit: PermitResponse;
     permits: PermitSummaryResponse[];
     sites?: SiteSummaryResponse[];
     inspections?: InspectionResponse[];
