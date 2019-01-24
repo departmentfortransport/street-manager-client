6ad873f513b5bffc0cfe723aff74935039ea1441
shaung <s.ganley@kainos.com>
2019-01-24 15:17:22 +0000

    removind non-required reinspection id

--- dist/interfaces/inspectionSummaryResponse.d.ts
+++ dist/interfaces/inspectionSummaryResponse.d.ts
@@ -3,7 +3,6 @@ export interface InspectionSummaryResponse {
     inspection_date_time: Date;
     inspection_response_type: InspectionResponseType;
     inspection_reference_number?: string;
-    reinspection_id?: number;
     inspection_type?: InspectionType;
     inspection_category?: InspectionCategory;
     inspection_outcome?: InspectionOutcome;
