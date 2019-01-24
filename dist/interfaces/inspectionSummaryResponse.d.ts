605f4b68a5e719d98e2518738bae3d47f3643102
shaung <s.ganley@kainos.com>
2019-01-24 15:51:08 +0000

    making reinspection date time optional field to differentiate on frontend

--- dist/interfaces/inspectionSummaryResponse.d.ts
+++ dist/interfaces/inspectionSummaryResponse.d.ts
@@ -1,10 +1,10 @@
 import { InspectionCategory, InspectionOutcome, InspectionType, InspectionResponseType } from './referenceTypes';
 export interface InspectionSummaryResponse {
-    inspection_date_time: Date;
+    inspection_date: Date;
     inspection_response_type: InspectionResponseType;
     inspection_reference_number?: string;
     inspection_type?: InspectionType;
     inspection_category?: InspectionCategory;
     inspection_outcome?: InspectionOutcome;
-    inspection_date?: Date;
+    reinspection_date_time?: Date;
 }
