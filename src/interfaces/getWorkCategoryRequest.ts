5346c9f961824454e88ceeb53775e4d72f803a5a
Matthew O'Reilly <m.oreilly@kainos.com>
2019-02-18 19:24:35 +0000

    SM-613: Update GetWorkCategoryRequest date properties.

--- src/interfaces/getWorkCategoryRequest.ts
+++ src/interfaces/getWorkCategoryRequest.ts
@@ -2,8 +2,8 @@ import { WorkType } from './referenceTypes'
 
 export interface GetWorkCategoryRequest {
   work_type: WorkType,
-  start_date?: Date,
-  end_date?: Date,
+  proposed_start_date?: Date,
+  proposed_end_date?: Date,
   is_ttro_required?: boolean,
   immediate_risk?: boolean,
   work_reference_number?: string
