c15f64d6778081c616c059d5dd4591989c3892c8
Paul Wright <p.wright@kainos.com>
2019-03-26 09:26:35 +0000

    SM-2157: Allow granting work extensions with a duration challenge

--- dist/interfaces/permitAlterationStatusUpdateRequest.d.ts
+++ dist/interfaces/permitAlterationStatusUpdateRequest.d.ts
@@ -3,4 +3,5 @@ export interface PermitAlterationStatusUpdateRequest {
     alteration_status: AlterationStatus;
     assessment_comments?: string;
     assessment_discount?: number;
+    reasonable_period_end_date?: Date;
 }
--- src/interfaces/permitAlterationStatusUpdateRequest.ts
+++ src/interfaces/permitAlterationStatusUpdateRequest.ts
@@ -3,5 +3,6 @@ import { AlterationStatus } from './referenceTypes'
 export interface PermitAlterationStatusUpdateRequest {
   alteration_status: AlterationStatus,
   assessment_comments?: string,
-  assessment_discount?: number
+  assessment_discount?: number,
+  reasonable_period_end_date?: Date
 }
