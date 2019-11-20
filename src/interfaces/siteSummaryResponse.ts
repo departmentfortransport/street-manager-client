6b8cab42a775666d899e9a50d00d238dd77cc207
Eoin <e.mccoy@kainos.com>
2019-11-20 16:35:17 +0000

    Remove commas

--- src/interfaces/siteSummaryResponse.ts
+++ src/interfaces/siteSummaryResponse.ts
@@ -2,12 +2,12 @@ import { ReinstatementStatus, ReinstatementType } from './referenceTypes'
 import { SiteDetails } from './siteDetails'
 
 export interface SiteSummaryResponse extends SiteDetails {
-  permit_reference_number?: string,
-  depth?: number,
-  length?: number,
-  width?: number,
-  completed_date: Date,
-  end_date: Date,
+  permit_reference_number?: string
+  depth?: number
+  length?: number
+  width?: number
+  completed_date: Date
+  end_date: Date
   site_status: ReinstatementStatus
   reinstatement_type: ReinstatementType
   number_of_holes?: number
