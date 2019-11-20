0d6d872bfadf45191248747e3461faeff5d65815
Eoin <e.mccoy@kainos.com>
2019-11-20 12:11:15 +0000

    Remove permit_id and replace with permit_reference_number where required

--- src/interfaces/permitResponse.ts
+++ src/interfaces/permitResponse.ts
@@ -6,7 +6,6 @@ import { PermitSummaryResponse } from './permitSummaryResponse'
 import { PermitAlterationSummaryResponse } from './permitAlterationSummaryResponse'
 
 export interface PermitResponse extends PermitSummaryResponse {
-  permit_id: number
   permit_reference_number: string
   work_id: number
   work_reference_number: string
