5d2b0b83acfbd481c3d0fc8116717895756f0e82
Paul Wright <p.wright@kainos.com>
2019-12-04 11:23:05 +0000

    SM-4336: Add road category and works coordinates to work response
    This is to allow prepopulation when creating a permit on historic works

--- src/interfaces/permitCreateRequest.ts
+++ src/interfaces/permitCreateRequest.ts
@@ -3,5 +3,6 @@ import { PermitRequest } from './permitRequest'
 
 export interface PermitCreateRequest extends PermitRequest {
   permit_asds?: PermitASD[]
+  /** Must consist of 3 positive whole numbers */
   workstream_prefix?: string
 }
