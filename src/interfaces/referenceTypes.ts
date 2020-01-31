2d0251fce1a58d1028718b9fa17dab6dc90aba79
Cormac Curran <c.curran@kainos.com>
2020-01-31 08:58:33 +0000

    feature/SM-4467: update Audit Event enum
    add workstream access updated to AuditEvent enum

--- src/interfaces/referenceTypes.ts
+++ src/interfaces/referenceTypes.ts
@@ -386,7 +386,8 @@ export enum AuditEvent {
   section_81_cancelled = 'section_81_cancelled',
   support_desk_action = 'support_desk_action',
   permit_hs2_acknowledged = 'permit_hs2_acknowledged',
-  geographical_area_created = 'geographical_area_created'
+  geographical_area_created = 'geographical_area_created',
+  workstream_access_updated = 'workstream_access_updated'
 }
 
 export enum ASDCode {
