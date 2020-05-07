4ae4bbf3fe55500211530d5dcd834b7170ed0029
Matthew O'Reilly <m.oreilly@kainos.com>
2020-05-07 15:56:12 +0100

    bug/SM-5458: Set inspection_category property as optional in response models, as it isn't defined if the inspection_type is section_81.

--- dist/interfaces/inspectionSummaryResponse.d.ts
+++ dist/interfaces/inspectionSummaryResponse.d.ts
@@ -4,7 +4,7 @@ export interface InspectionSummaryResponse {
     inspection_response_type: InspectionResponseType;
     inspection_reference_number?: string;
     inspection_type: InspectionType;
-    inspection_category: InspectionCategory;
+    inspection_category?: InspectionCategory;
     inspection_outcome?: InspectionOutcome;
     reinspection_date_time?: Date;
 }
