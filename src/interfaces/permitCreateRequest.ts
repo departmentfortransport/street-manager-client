b0e9188648b02539e6cf6b76e443897667be6d5a
Cormac Curran <c.curran@kainos.com>
2020-08-05 12:28:47 +0100

    feature/SM-6036: add hs2 fields to PermitAlterationCreateRequest
    Move fields from PermitCreateRequest to PermitRequest as this is share by both
    the create permit endpoint and the creapte permit alteration endpoint
    
    Fields that have been moved:
     - hs2_consultation_requested_response_date
     - hs2_highway_emails

--- src/interfaces/permitCreateRequest.ts
+++ src/interfaces/permitCreateRequest.ts
@@ -6,14 +6,7 @@ export interface PermitCreateRequest extends PermitRequest {
   workstream_prefix?: string
   /** Required if promoter_swa_code = '7374' */
   hs2_in_act_limits?: boolean
-  /** Date must occur today or a date in the future */
-  hs2_consultation_requested_response_date?: Date
   /** Required if work_type = 'hs2_highway_works' and hs2_in_act_limits = true */
   hs2_highway_exemption?: HS2HighwayExemption
-  /** Array Max length 2 items
-   * Array values must be valid email addresses
-   * Array values max length 100 characters
-   */
-  hs2_highway_emails?: string[]
   is_covid_19_response?: boolean
 }
